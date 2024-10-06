781ad52042c26e1e9d08700ed0fa38c5333c62da
Derek <RumbleInCybertron@users.noreply.github.com>
2024-10-06 21:17:49 +0900

    refactor img del

--- api/app/crud.py
+++ api/app/crud.py
@@ -3,6 +3,7 @@ import json
 import os
 
 DATA_FILE = "data/images.json"
+IMAGE_DIR = "images/"
 
 def load_data():
   # Check if the file exists and is not empty
@@ -11,7 +12,10 @@ def load_data():
   
   # Load image data from the JSON file.
   with open(DATA_FILE, "r") as file:
-    return json.load(file)
+    try:
+      return json.load(file)
+    except json.JSONDecodeError:
+      return []
   
 def save_data(data):
   # Save updated image data to the JSON file.
@@ -45,8 +49,38 @@ def get_all_images():
     return load_data()
 
 def delete_image(image_id):
-    #Delete an image by its ID.
+    # Delete an image by its ID.
     data = load_data()
-    updated_data = [image for image in data if image["id"] != image_id]
-    save_data(updated_data)
+    
+    image_to_del = next((i for i in data if i['id'] == image_id), None)
+    
+    if image_to_del:
+      # Delete the physical img file from the file system
+      # image_path = os.path.join(IMAGE_DIR, image_to_del['filepath'])
+      image_filename = image_to_del['filepath']
+      compressed_image_filename = image_to_del['compressed_filepath']
+      
+      # Track the success of file deletions
+      original_deleted = False
+      compressed_deleted = False
+      
+      # Check if image file exists and remove it
+      if os.path.exists(image_filename):
+        os.remove(image_filename)
+        original_deleted = True
+        
+      # Check if compressed image file exists and remove it
+      if os.path.exists(compressed_image_filename):
+        os.remove(compressed_image_filename)
+        compressed_deleted = True
+        
+      # Remove the img metadata from the json data
+      if original_deleted and compressed_deleted:
+        data.remove(image_to_del)
+        save_data(data)
+      else:
+        print("One or both files could not be deleted. Metadata not removed.")
+    
+    # updated_data = [image for image in data if image["id"] != image_id]
+    # save_data(updated_data)
     return {"message": "Image deleted successfully"}
